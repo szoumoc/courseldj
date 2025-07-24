4b49ce01a0e7d980eb963fbff1a4bc951534946f
szoumoc <soumosaha4327@gmail.com>
2025-07-24 17:30:03 +0530

    Storing Image files locally

--- src/core/settings.py
+++ src/core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOCAL_CDN = BASE_DIR.parent / "local-cdn"
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = LOCAL_CDN / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
